a00001250e5c6415d3c4a2c8a3d192ce280c8c9a
AworThales <thals4jesus@gmail.com>
2023-12-20 17:58:29 +0000

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python3
-"""
-Web file
-"""
+"""Redis Module."""
 import requests
 import redis
 from functools import wraps
+from typing import Callable
 
-store = redis.Redis()
+cache = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a Url is accessed """
+def store_cache(method: Callable) -> Callable:
+    """Cache a value for 10 seconds."""
     @wraps(method)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        """The function wrapper."""
+        if cache.get(url):
+            return cache.get(url).decode()
+        result = method(url)
+        cache.setex(url, 10, result)
+        return result
     return wrapper
 
 
-@count_url_access
+def track_url(method: Callable) -> Callable:
+    """Cache a value for 10 seconds."""
+    @wraps(method)
+    def wrapper(url):
+        """The function wrapper."""
+        cache.incr(f'count:{url}')
+        return method(url)
+    return wrapper
+
+
+@store_cache
+@track_url
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """Get the result of a webpage."""
+    if cache.get(url):
+        return cache.get(url).decode()
+    result = requests.get(url)
+    return result.text
